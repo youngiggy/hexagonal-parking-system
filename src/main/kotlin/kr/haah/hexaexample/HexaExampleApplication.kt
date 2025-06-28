71d39980bb1c9c08fd59353b68b3e478202783a4
Youngik Joo <youngik.joo@meshkorea.net>
2025-06-28 12:15:54 +0900

    initial commit

---
+++ src/main/kotlin/kr/haah/hexaexample/HexaExampleApplication.kt
@@ -0,0 +1,11 @@
+package kr.haah.hexaexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HexaExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<HexaExampleApplication>(*args)
+}
---
+++ src/test/kotlin/kr/haah/hexaexample/HexaExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.haah.hexaexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HexaExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
